94792b5e6beeca2db80814fc38aca9b58735a14f
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-08-19 12:56:38 -0700

    create requests object for api calls

--- src/request.js
+++ src/request.js
@@ -1,1 +1,14 @@
 const APIKEY = "c30dc1203f1e7b29895846f805a7da33";
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${APIKEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${APIKEY}&with_network=213`,
+  fetchTopRate: `/movie/top_rated?api_key=${APIKEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${APIKEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${APIKEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${APIKEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${APIKEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${APIKEY}&with_genres=99`,
+};
+
+export default requests;
