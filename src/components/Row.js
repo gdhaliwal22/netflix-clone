fb201f28f4725abcd91c59c0c9309915abfc1bc1
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-08-22 15:49:17 -0700

    create useEffect function to fetch movies

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,9 +1,19 @@
 import React, { useState, useEffect } from "react";
+import axios from "../axios";
 
-function Row({ title, fetchURL }) {
+function Row({ title, fetchUrl }) {
   const [movies, setMovies] = useState([]);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    // if [], run once when row loads, and don't run again
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+  console.log(movies);
   return (
     <div>
       {/* {title} */}
