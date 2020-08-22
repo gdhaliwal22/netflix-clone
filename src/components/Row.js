9c3510caba0e38d088db3baecc4815d9a0362d45
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-08-22 16:19:35 -0700

    rendering row of poster images

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "../axios";
 
+const base_url = "https//image.tmdb.org/t/p/origial/";
+
 function Row({ title, fetchUrl }) {
   const [movies, setMovies] = useState([]);
 
@@ -13,11 +15,19 @@ function Row({ title, fetchUrl }) {
     }
     fetchData();
   }, [fetchUrl]);
-  console.log(movies);
+
   return (
-    <div>
-      {/* {title} */}
-      {/* {container -> poster} */}
+    <div className="row">
+      <h2>{title}</h2>
+      <div className="row_poster(s)">
+        {movies.map((movie, index) => (
+          <img
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.name}
+            key={index}
+          />
+        ))}
+      </div>
     </div>
   );
 }
