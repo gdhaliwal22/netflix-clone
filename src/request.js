f561afc6603776900ce2f894d4ba06fcfc496b81
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-08-22 17:07:56 -0700

    increase css and hover for netflix originals, movie tiles larger

--- src/request.js
+++ src/request.js
@@ -3,7 +3,7 @@ const APIKEY = "c30dc1203f1e7b29895846f805a7da33";
 const requests = {
   fetchTrending: `/trending/all/week?api_key=${APIKEY}&language=en-US`,
   fetchNetflixOriginals: `/discover/tv?api_key=${APIKEY}&with_network=213`,
-  fetchTopRate: `/movie/top_rated?api_key=${APIKEY}&language=en-US`,
+  fetchTopRated: `/movie/top_rated?api_key=${APIKEY}&language=en-US`,
   fetchActionMovies: `/discover/movie?api_key=${APIKEY}&with_genres=28`,
   fetchComedyMovies: `/discover/movie?api_key=${APIKEY}&with_genres=35`,
   fetchHorrorMovies: `/discover/movie?api_key=${APIKEY}&with_genres=27`,
