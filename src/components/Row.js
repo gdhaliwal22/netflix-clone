e94fcc6eb959e014425af3a611758afa36a8ecf3
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-08-23 12:18:20 -0700

    fixed 400 error movies images now rendering

--- src/components/Row.js
+++ src/components/Row.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "../axios";
 import "../Row.css";
 
-const base_url = "https://image.tmdb.org/t/p/origial";
+const base_url = "https://image.tmdb.org/t/p/original";
 
 function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
