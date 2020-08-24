dc742b50ce3113e24b1d84dcb5c2f0aec2fcc451
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-08-24 12:05:03 -0700

    created handeclick function to get trailer url

--- src/components/Row.js
+++ src/components/Row.js
@@ -2,11 +2,13 @@ import React, { useState, useEffect } from "react";
 import axios from "../axios";
 import "../Row.css";
 import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 const base_url = "https://image.tmdb.org/t/p/original";
 
 function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   useEffect(() => {
     // if [], run once when row loads, and don't run again
@@ -24,6 +26,19 @@ function Row({ title, fetchUrl, isLargeRow }) {
     playerVars: {
       autoplay: 1,
     },
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
+    }
   };
   return (
     <div className="row">
