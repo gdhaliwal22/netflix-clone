4f2add51e51f1ddbee0b529eeb93fed2e288d84a
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-08-24 09:49:53 -0700

    created inline style object inside header in Banner component

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -13,15 +13,28 @@ function Banner() {
           Math.floor(Math.random() * request.data.results.length - 1)
         ]
       );
+      return request;
     }
     fetchData();
   }, []);
+  //   console.log(movie);
   return (
-    <header>
-      {/* Background image */}
-      {/* {title} */}
-      {/* {div > 2 buttons} */}
-      {/* {description} */}
+    <header
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundPosition: "center center",
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+      }}
+    >
+      <div className="banner__contents">
+        {""}
+        {/* Background image */}
+        {/* {title} */}
+        <h1></h1>
+        {/* {div > 2 buttons} */}
+        {/* {description} */}
+      </div>
     </header>
   );
 }
