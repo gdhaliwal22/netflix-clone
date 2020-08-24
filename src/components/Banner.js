4b27e570dd5080b564f18160bb5b50e916e31fd1
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-08-24 10:33:06 -0700

    created function to truncate movie description in banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -18,7 +18,10 @@ function Banner() {
     }
     fetchData();
   }, []);
-  //   console.log(movie);
+  console.log(movie);
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
   return (
     <header
       className="banner"
@@ -36,7 +39,10 @@ function Banner() {
           <button className="banner__button">Play</button>
           <button className="banner__button">My List</button>
         </div>
-        <h1 className="banner__description">{movie?.overview}</h1>
+        <h1 className="banner__description">
+          {movie?.overview}
+          {truncate(movie?.overview, 150)}
+        </h1>
       </div>
     </header>
   );
